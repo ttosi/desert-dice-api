f3a93a6abf99269c8499fec8681ca860363a88e9
ttosi <ttosi519@gmail.com>
2025-05-25 21:15:41 -0700

    products api

--- api/products.js
+++ api/products.js
@@ -1,21 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const { fetchAll, fetchOne } = require("../database/database");
+const { fetchAll } = require("../database/database");
 
-router.get("/", async (req, res, next) => {
-  const q = await fetchAll(
-    "SELECT * FROM productCategory WHERE isActive = 1 ORDER BY sequence;"
-  );
+/* get all products (does not return images) */
+router.get("/", async (req, res) => {
+  const q = await fetchAll("SELECT * FROM product WHERE sold IS NULL;");
   res.json(q);
-  next();
 });
 
-router.get("/:id", async (req, res, next) => {
-  const q = await fetchOne(
-    `SELECT * FROM product WHERE id = ${req.params.id} ;`
+/* get 3 latest featured products */
+router.get("/featured", async (req, res) => {
+  const q = await fetchAll(
+    `SELECT * FROM product WHERE featured = 1 AND sold IS NULL ORDER BY created DESC LIMIT 3;`
   );
-  res.json(q);
-  next();
+  q?.length ? res.json(q) : res.status(404);
+});
+
+/* get 3 latest products */
+router.get("/latest", async (req, res) => {
+  const q = await fetchAll(
+    `SELECT * FROM product WHERE sold IS NULL ORDER BY created DESC LIMIT 3;`
+  );
+  q?.length ? res.json(q) : res.status(404);
+});
+
+router.get("/:id", async (req, res) => {
+  const q = await fetchAll(
+    `SELECT p.id, p.name, p.description, p.cost, p.discount, pi.path
+     FROM product p 
+     JOIN productImage pi on p.id = pi.productId
+     WHERE p.id = ? AND sold IS NULL;`,
+    [req.params.id]
+  );
+
+  if (!q?.length) {
+    res.status(404).end();
+  } else {
+    res.json({
+      name: q[0].name,
+      description: q[0].description,
+      cost: q[0].cost,
+      discount: q[0].discount,
+      images: q.map((i) => i.path),
+    });
+  }
 });
 
 module.exports = router;
