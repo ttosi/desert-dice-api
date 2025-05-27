b11aa1c69e68c63e7f05ba03d07376c40003f3a9
ttosi <ttosi519@gmail.com>
2025-05-27 16:43:48 -0700

    products api

--- api/products.js
+++ api/products.js
@@ -5,10 +5,14 @@ const db = require("../database/database");
 /* get all products */
 router.get("/", async (req, res) => {
   const rows = await db.fetchAll(
-    `SELECT p.id, p.name, p.description, p.cost, p.discount, pi.path AS images
-     FROM product p 
-     JOIN productImage pi on p.id = pi.productId
-     WHERE p.sold IS NULL ORDER BY p.created ASC;`
+    `SELECT p.id, p.name, p.description, p.cost / 100.0 as cost, p.discount, pi.path AS images
+     FROM product p
+     LEFT JOIN productImage pi ON p.id = pi.productId
+     WHERE p.id IN (
+       SELECT id FROM product
+       WHERE sold IS NULL
+     )
+     ORDER BY p.created ASC;`
   );
 
   if (!rows?.length) return res.status(404).end();
@@ -18,7 +22,7 @@ router.get("/", async (req, res) => {
 /* get the 3 latest featured products */
 router.get("/featured", async (req, res) => {
   const rows = await db.fetchAll(
-    `SELECT p.id, p.name, p.description, p.cost, p.discount, pi.path AS images
+    `SELECT p.id, p.name, p.description, p.cost / 100.0, p.discount, pi.path AS images
      FROM product p
      LEFT JOIN productImage pi ON p.id = pi.productId
      WHERE p.id IN (
@@ -35,7 +39,7 @@ router.get("/featured", async (req, res) => {
 /* get the 3 latest products */
 router.get("/latest", async (req, res) => {
   const rows = await db.fetchAll(
-    `SELECT p.id, p.name, p.description, p.cost, p.discount, pi.path AS images
+    `SELECT p.id, p.name, p.description, p.cost / 100.0, p.discount, pi.path AS images
      FROM product p
      LEFT JOIN productImage pi ON p.id = pi.productId
      WHERE p.id IN (
@@ -51,10 +55,14 @@ router.get("/latest", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const rows = await db.fetchAll(
-    `SELECT p.id, p.name, p.description, p.cost, p.discount, pi.path AS images
-     FROM product p 
-     JOIN productImage pi on p.id = pi.productId
-     WHERE p.id = ? AND p.sold IS NULL;`,
+    `SELECT p.id, p.name, p.description, p.cost / 100.0, p.discount, pi.path AS images
+     FROM product p
+     LEFT JOIN productImage pi ON p.id = pi.productId
+     WHERE p.id IN (
+       SELECT id FROM product
+       WHERE sold IS NULL AND p.id = ?
+     )
+     ORDER BY p.created DESC;`,
     [req.params.id]
   );
 
