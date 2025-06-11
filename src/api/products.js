90c5f7e152b33750cf79b5a484022e96c845655b
ttosi <ttosi519@gmail.com>
2025-06-11 13:32:29 -0700

    change db path

--- src/api/products.js
+++ src/api/products.js
@@ -109,7 +109,7 @@ router.get("/:id", async (req, res) => {
     `SELECT 
         p.id, p.name AS productName, p.description, p.cover_image_path,
         p.cover_price / 100, p.created_at, po.id AS productOptionId,
-        po.name AS productOptionName, po.price / 100 AS optionPrice, po.notes
+        po.name AS productOptionName, po.price / 100 AS optionPrice, po.notes,
         pi.id as productImageId, pi.path, pi.is_thumbnail
      FROM product p
      LEFT JOIN product_option po ON p.id = po.product_id
